998e551288c1471e30aeae7f0e2a5c66c8c5878d
Angelus Hein <angelushein2004@gmail.com>
2025-05-19 18:23:07 -0500

    separated code into different files

--- makefile
+++ makefile
@@ -9,7 +9,7 @@ src_files := $(shell find src -type f)
 
 build: $(ui_files) $(src_files)
 	mkdir -p $(build_path)
-	cp -r ui/images $(build_path)
+	cp -r ui/assets $(build_path)
 	blueprint-compiler compile $(ui_path) > $(build_path)/main.ui
 	gcc src/*.c -o $(build_path)/$(application_name) $(build_flags)
 
--- src/main.c
+++ src/main.c
@@ -1,47 +1,15 @@
 #include "gtk-4.0/gtk/gtk.h"
-#include "libgen.h"
 
-GtkWidget* window;
-
-GtkBuilder* builder;
-
-// Needs error checking
-// does not set null terminator
-void get_exe_path(char* buffer, int buffer_max) {
-    int len = readlink("/proc/self/exe", buffer, buffer_max);
-    buffer[len] = '\0';
-}
-
-void get_ui_path(char* buffer, int buffer_max) {
-    get_exe_path(buffer, buffer_max);
-    char* dir = dirname(buffer);
-
-    sprintf(buffer, "%s%s", dir, "/main.ui");
-}
-
-void activate (GtkApplication *app, gpointer user_data) {
-    int buffer_max = 428;
-    char* ui_path_buffer[buffer_max];
-    get_ui_path(ui_path_buffer, buffer_max);
-    
-    builder = gtk_builder_new_from_file(ui_path_buffer);
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
-
-    gtk_window_set_application(GTK_WINDOW(window), app);
-
-    // gtk_window_set_title (GTK_WINDOW (window), "Window");
-    // gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-    gtk_window_present(GTK_WINDOW (window));
-}
+#include "application.h"
 
 int main (int argc, char **argv) {
-    GtkApplication *app;
     int status;
 
     app = gtk_application_new("RPS.PROJECT", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+
+    app_exit();
     
     return status;
 }
